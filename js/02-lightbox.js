0ffc335a5eebbd9f5d87c9b0200c69b64a680a4c
Stefan Dunikovskyi <stefan090304@gmail.com>
2022-06-02 20:45:01 +0200

    add secondGallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryWrapper = document.querySelector(".gallery");
+
+const createMarkup = () => {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`
+    )
+    .join("");
+};
+function onClickImg(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  var lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+  console.log(lightbox);
+}
+
+galleryWrapper.innerHTML = createMarkup();
+galleryWrapper.addEventListener("click", onClickImg);
